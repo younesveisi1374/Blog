acf83e5af87824f07ef8e19551922b96b31cbc56
younesveisi1374 <younesveisi1374@gmail.com>
2023-12-12 23:44:34 +0330

    change views in app for class-base and more simple

--- twitter/views.py
+++ twitter/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
 
 # def messageView(request):
 #    return render(request,'twitter/index.html',{'name': 'Younes'})
 
 
-class MessageView(View):
-    def get(self, request):
-        return render(request, 'twitter/index.html', {'name': 'Younes'})
+class MessageView(TemplateView):
+    template_name = 'twitter/index.html'
+    #def get(self, request):
+    #    return render(request, 'twitter/index.html', {'name': 'Younes'})
