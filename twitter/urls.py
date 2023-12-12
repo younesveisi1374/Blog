d9f8a2b8a4d465a0782058b12c0963a0743d63f5
younesveisi1374 <younesveisi1374@gmail.com>
2023-12-12 23:41:37 +0330

    change views in app for class-base

--- twitter/urls.py
+++ twitter/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.messageView,name = 'message')
+    path('',views.MessageView.as_view(),name = 'message')
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-def messageView(request):
-    return render(request,'twitter/index.html',{'name': 'Younes'})
+# def messageView(request):
+#    return render(request,'twitter/index.html',{'name': 'Younes'})
+
+
+class MessageView(View):
+    def get(self, request):
+        return render(request, 'twitter/index.html', {'name': 'Younes'})
