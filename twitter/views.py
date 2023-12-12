4463e225ff1d25633f163654ad6a732acd278fe9
younesveisi1374 <younesveisi1374@gmail.com>
2023-12-12 23:28:04 +0330

    show first message in webpage

--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def messageView(request):
+    return render(request,'twitter/index.html',{'name': 'Younes'})
