61031f2475b697902f899ee1fc791e57878d48e1
younesveisi1374 <younesveisi1374@gmail.com>
2023-12-17 11:04:17 +0330

    create message with admin panel and show into webpage with django methods

--- twitter/views.py
+++ twitter/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
+from .models import Message
 
 # def messageView(request):
 #    return render(request,'twitter/index.html',{'name': 'Younes'})
 
 
-class MessageView(TemplateView):
+class MessageView(ListView):
+    model = Message
     template_name = 'twitter/index.html'
-    #def get(self, request):
+    # def get(self, request):
     #    return render(request, 'twitter/index.html', {'name': 'Younes'})
