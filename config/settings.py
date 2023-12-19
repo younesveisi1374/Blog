644f273725354c7a8d3bf5268db011b6608874a3
younesveisi1374 <younesveisi1374@gmail.com>
2023-12-19 22:25:59 +0330

    add configs for reset password

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
