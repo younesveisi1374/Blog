e98ff408344844b494451d26a9d3bbf1996808d6
younesveisi1374 <younesveisi1374@gmail.com>
2023-12-21 11:44:04 +0330

    add ' add post' page in webpage for twitter

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, NewPostView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('post/new',NewPostView.as_view(), name = 'newpost'),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView,ListView, CreateView
 from .models import Post
 from django.contrib.auth import logout
+from .forms import PostForm
+from django.urls import reverse_lazy
+
 
 def logout_view(request):
     logout(request)
@@ -17,3 +20,11 @@ class HomeView(ListView):
     template_name = 'twitter/index.html'
     # def get(self, request):
     #    return render(request, 'twitter/index.html', {'name': 'Younes'})
+
+
+class NewPostView(CreateView):
+    model = Post
+    template_name = 'twitter/post_new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('home')
+    # fields = ['title','excerpt','body','author','date','photo']
