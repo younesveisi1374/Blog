1baebca33d84a3d0fceac5a1f76e9326bb8d5d0f
younesveisi1374 <younesveisi1374@gmail.com>
2023-12-21 14:36:51 +0330

    update and delete for posts

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import HomeView, NewPostView, PostDetailView
+from .views import HomeView, NewPostView, PostDetailView, PostUpdateView, PostDeleteView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('post/new', NewPostView.as_view(), name='newpost'),
-    path('post/<int:pk>',PostDetailView.as_view(),name = 'post_detail'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/update/<int:pk>',PostUpdateView.as_view(),name = 'post_update'),
+    path('post/delete/<int:pk>',PostDeleteView.as_view(),name = 'post_delete'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- twitter/views.py
+++ twitter/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView, CreateView,DetailView
+from django.views.generic import TemplateView, ListView, CreateView, DetailView
 from .models import Post
 from django.contrib.auth import logout
 from .forms import PostForm
 from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView, DeleteView
 
 
 def logout_view(request):
     logout(request)
-    
+
     # Redirect to a success page.
 # def messageView(request):
 #    return render(request,'twitter/index.html',{'name': 'Younes'})
@@ -33,3 +34,15 @@ class NewPostView(CreateView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'twitter/post_single.html'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'twitter/post_update.html'
+    fields = ['title', 'excerpt', 'body', 'photo']
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'twitter/post_delete.html'
+    success_url = reverse_lazy('home')
