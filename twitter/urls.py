54ae7d43664e26f3745d8fa84eda414405328492
younesveisi1374 <younesveisi1374@gmail.com>
2023-12-21 12:49:23 +0330

    show posts in homepage

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import HomeView, NewPostView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('post/new',NewPostView.as_view(), name = 'newpost'),
+    path('post/new', NewPostView.as_view(), name='newpost'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
