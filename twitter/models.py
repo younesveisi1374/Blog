4a2d8c6c2d495a4ec163334b91711b2e779503ba
younesveisi1374 <younesveisi1374@gmail.com>
2024-01-02 20:18:42 +0330

    add comment for posts and change urls for show better comments and count comments

--- twitter/models.py
+++ twitter/models.py
@@ -1,20 +1,31 @@
 from django.db import models
 from datetime import date
 from django.urls import reverse
-# Create your models here.
+from django.utils import timezone
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     excerpt = models.TextField()
     body = models.TextField()
-    author = models .ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    author = models .ForeignKey(
+        'accounts.CustomUser', on_delete=models.CASCADE)
     date = models.DateField(default=date.today)
-    photo = models.ImageField(upload_to = 'photo/%Y/%m/%d')
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    author = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField(null=False, blank=False)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.body
     
--- twitter/views.py
+++ twitter/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, CreateView, DetailView
+from django.views.generic import TemplateView, ListView, CreateView, DetailView, View, FormView
 from .models import Post
 from django.contrib.auth import logout
-from .forms import PostForm
-from django.urls import reverse_lazy
+from .forms import PostForm, CommentForm
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import UpdateView, DeleteView
 from django.core.paginator import Paginator
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.detail import SingleObjectMixin
 
 
 def logout_view(request):
@@ -50,9 +51,82 @@ class NewPostView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostDetailView(DetailView):
+class CommentGet(DeleteView):
     model = Post
     template_name = 'twitter/post_single.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the parent class's `get_context_data` method to retrieve the existing context.
+        # `super()` allows us to call methods of the base class in derived classes.
+        context = super().get_context_data(**kwargs)
+
+        # Add a new entry to the context dictionary with the key 'form'.
+        # The value associated with 'form' is an instance of CommentForm.
+        # CommentForm is likely a form class that handles user comments.
+        context["form"] = CommentForm()
+
+        # Return the updated context dictionary.
+        # This context dictionary now includes all the data from the parent class,
+        # as well as the new 'form' data we added for the CommentForm.
+        return context
+
+
+# Define a class named CommentPost which inherits from SingleObjectMixin and FormView.
+class CommentPost(SingleObjectMixin, FormView):
+    # Set the model that this view will operate on to 'Post'.
+    model = Post
+    
+    # Set the form_class attribute to 'CommentForm', specifying what form to use.
+    form_class = CommentForm
+    
+    # Define the template file that will be used when rendering the view.
+    template_name = 'twitter/post_single.html'
+
+    # Override the 'post' method to handle HTTP POST requests.
+    def post(self, request, *args, **kwargs):
+        # Retrieve the object related to this view.
+        # In this context, it's likely to be the Post instance that is being commented on.
+        self.object = self.get_object()
+        
+        # Call the parent class's 'post' method with all arguments to continue handling the POST request.
+        return super().post(request, *args, **kwargs)
+
+    # Override the 'form_valid' method which is called when valid form data has been POSTed.
+    def form_valid(self, form):
+        # Create a new tweet object from the form but don't save it to the database yet (commit=False).
+        tweet = form.save(commit=False)
+        
+        # Set the 'post' attribute of the tweet to the object we got earlier (the post being commented on).
+        tweet.post = self.object
+        
+        # Assign the currently logged-in user as the author of the tweet.
+        tweet.author = self.request.user
+        
+        # Now save the tweet object with all the added information into the database.
+        tweet.save()
+        
+        # After saving the tweet, call the superclass's form_valid method to continue processing.
+        return super().form_valid(form)
+
+    # Override 'get_success_url' to provide the URL to redirect to after successful form submission.
+    def get_success_url(self) -> str:
+        # Get the current object again, which is likely to be a post instance here.
+        post = self.get_object()
+        
+        # Use Django's reverse function to generate a URL for the single post view.
+        # This assumes that there exists a URL pattern named "twitter/post_single.html" expecting a 'pk' keyword argument.
+        return reverse("post_detail", kwargs={'pk': post.pk})
+
+
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
 
 
 class PostUpdateView(UpdateView):
